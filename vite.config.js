76c7bf6becabf481fe22ddf2bd24f7ac5d3eca58
Artem <artemgarbach99@gmail.com>
2025-11-28 13:38:26 +0300

    Update source before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
 	base: '/scand/',
 	build: {
-		outDir: 'dist'
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				blocks: resolve(__dirname, 'blocks.html')
+			}
+		}
 	}
 })
